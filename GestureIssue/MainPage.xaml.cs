a9f3d957955fcc7a893b3f033f042ff6a17c2122
NavaneethS <37859978+NavaneethS@users.noreply.github.com>
2023-03-23 14:23:07 +0530

    Created the repository and added the files

---
+++ GestureIssue/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+namespace GestureIssue;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+
+        ContentView contentView = new ContentView();
+        PinchGestureRecognizer pinchGestureRecognizer = new PinchGestureRecognizer();
+        pinchGestureRecognizer.PinchUpdated += (sender, e) =>
+        {
+            Debug.WriteLine("PinchUpdated");
+        };
+        contentView.GestureRecognizers.Add(pinchGestureRecognizer);
+        Content = contentView;
+
+
+        Grid childView = new Grid();
+        TapGestureRecognizer tapGestureRecognizer = new TapGestureRecognizer();
+        tapGestureRecognizer.Tapped += (sender, e) =>
+        {
+            Debug.WriteLine("Tapped");
+        };
+        childView.GestureRecognizers.Add(tapGestureRecognizer);
+        contentView.Content = childView;
+    }
+}
+
